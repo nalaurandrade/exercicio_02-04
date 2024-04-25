f9bf13b4d624549eedef653e04a4cf29b482bcff
Ana Laura Andrade <143561088+nalaurandrade@users.noreply.github.com>
2024-04-25 18:48:52 -0300

    Add files via upload

---
+++ atividade3/atividade3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] vetor = new int[20];
+
+        Console.WriteLine("Digite 20 numeros:");
+
+        for (int i = 0; i < 20; i++)
+        {
+            vetor[i] = int.Parse(Console.ReadLine());
+        }
+
+
+        for (int i = 0; i < 10; i++)
+        {
+            int temp = vetor[i];
+            vetor[i] = vetor[19 - i];
+            vetor[19 - i] = temp;
+        }
+
+
+        for (int i = 0; i < 20; i++)
+        {
+            Console.WriteLine($"N[{i}] = {vetor[i]}");
+        }
+    }
+}
